ac73eaa1d779ed977203b742fff9aaebd05a9aef
EC2 Default User <ec2-user@ip-172-31-8-63.ap-southeast-1.compute.internal>
2018-08-17 11:53:42 +0000

    pull

--- app/Http/Controllers/Api/SupplyController.php
+++ app/Http/Controllers/Api/SupplyController.php
@@ -25,20 +25,15 @@ class SupplyController extends Controller
         $user = JWTAuth::parseToken()->authenticate();
         $user->supplies()->detach();
 
-         $supplies = $request["supplies"];
-
-            foreach ($supplies as $supply) 
-            {
-                $newSupply = DB::table('supplies')->insert([
-                    'product_id' => $supply["product_id"],
-                    'harvesting_period_start' => Carbon::parse($supply["harvesting_period_start"], 'UTC')->setTimezone('Asia/Kuala_Lumpur'),
-                    'harvesting_period_end' => Carbon::parse($supply["harvesting_period_end"], 'UTC')->setTimezone('Asia/Kuala_Lumpur'),
-                    'harvest_frequency' => $supply["harvest_frequency"],
-                    'total_plants' => $supply["total_plants"],
-                    'total_farm_area' => $supply["total_farm_area"],
-                    'user_id' => $user->id
-                ]);
-            }
+        foreach ($request->all() as $supply) {
+            $user->supplies()->syncWithoutDetaching([$supply["product"]["id"] => [
+                'harvesting_period_start' => Carbon::parse($supply["harvestingPeriodStart"], 'UTC')->setTimezone('Asia/Kuala_Lumpur'),
+                'harvesting_period_end' => Carbon::parse($supply["harvestingPeriodEnd"], 'UTC')->setTimezone('Asia/Kuala_Lumpur'),
+                'harvest_frequency' => $supply["harvestFrequency"],
+                'total_plants' => $supply["totalPlants"],
+                'total_farm_area' => $supply["totalFarmArea"],
+            ]]);
+        }
 
         return response()->json([
             "data" => $user->supplies,
